e890b789ccee59ccec539ec07ffd258f0132dd06
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2025-02-08 20:34:13 -0300

    feat: add button to sign up route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-// import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <h1>hello world</h1>
+      <Link
+        href="/signup"
+        className="flex items-center gap-5 absolute top-4 right-4 rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+      >
+        Sign up
+      </Link>
     </>
   );
 }
