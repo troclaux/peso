3224001b4a826025b41751a58ecd4932c913665b
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2025-02-12 10:34:28 -0300

    feat: add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import Login from '@/components/Login'
+
+export default function LoginPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <Login />
+    </main>
+  )
+}
