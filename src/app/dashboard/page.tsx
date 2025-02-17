b0ac176812c6c874e3c1565d0a6d16de2febc56f
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2025-02-16 21:19:55 -0300

    chore: remove clerk auth provider across project

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,9 @@
-import { auth } from '@clerk/nextjs/server';
-import { SignedIn } from '@clerk/nextjs';
 
 export default async function Page() {
 
-  const { userId, redirectToSignIn } = await auth()
-  if (!userId) {
-    return redirectToSignIn();
-  } else {
-    return (
-
-      <SignedIn>
-        <h1 className="text-black">
-          Hello, {userId}
-        </h1>
-      </SignedIn >
-
-    );
-  }
+  return (
+    <>
+      <div>hello</div>
+    </>
+  );
 }
