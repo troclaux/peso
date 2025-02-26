72f830c8f49ca4b9bb518bca30855ea3e58bc791
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2025-02-26 16:44:41 -0300

    feat: dashboard now blocks unauthenticated users

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,23 @@
+"use client"
 
-export default async function Page() {
+import { useSession } from "next-auth/react"
 
-  return (
-    <>
-      <div>hello</div>
-    </>
-  );
+export default function Page() {
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirectTo: "/api/auth/signin"
+    }
+  });
+
+  if (session?.user) {
+    return (
+      <>
+        <h1>You are logged in, welcome!</h1>
+        <h3>{session?.user?.email}</h3>
+      </>
+    );
+  }
+
+  return <p>You are not logged in!</p>
 }
