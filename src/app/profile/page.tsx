a8ff716fc460743c47c2ab078b89e7f50eb6f5c9
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2025-02-27 08:18:09 -0300

    feat: add profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { redirect } from "next/navigation";
+import { useSession } from "next-auth/react";
+
+const Profile = () => {
+  const { data: session } = useSession()
+
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/Member");
+  }
+
+  return (
+    <div>
+      <p>{session?.user?.name}</p>
+      <p>{session?.user?.email}</p>
+    </div>
+  );
+};
+
+export default Profile;
