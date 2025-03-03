24c10f5854fe11752474f364f67fd3d89b5c7e85
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2025-03-03 11:15:20 -0300

    ci: add key-pair to allow access to ec2 instance

--- main.tf
+++ main.tf
@@ -103,6 +103,7 @@ resource "aws_instance" "app_instance" {
   subnet_id              = aws_subnet.main.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   associate_public_ip_address = true
+  key_name               = "my-key-pair"
 
   tags = {
     Name = "Peso"
