318dec67942c23853629729f50f98249bb8a7c5c
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2025-03-16 14:56:34 -0300

    feat: add secret to allow k8s to pull ecr private images

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-sudo kubectl delete secret peso-secrets && sudo kubectl create secret generic peso-secrets --from-env-file=.env.local
+AWS_PASSWORD=$(sudo aws ecr get-login-password --region sa-east-1)
+sudo kubectl delete secret regcred --ignore-not-found
+sudo kubectl create secret docker-registry regcred \
+  --docker-server=072216710152.dkr.ecr.sa-east-1.amazonaws.com \
+  --docker-username=AWS \
+  --docker-password="$AWS_PASSWORD"
+sudo kubectl delete secret peso-secrets --ignore-not-found
+sudo kubectl create secret generic peso-secrets --from-env-file=.env.local
+
 sudo kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.17.0/cert-manager.yaml
-sudo kubectl apply -f ./k8s/ec2-deployment.yaml
+sudo kubectl apply -f ~/k8s/ec2-deployment.yaml
 sudo kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
