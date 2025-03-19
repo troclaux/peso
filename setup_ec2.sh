363a9c2b5e398bb3aa7e94f8594a970c691502ee
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2025-03-19 09:33:29 -0300

    feat: add docker and docker compose to ec2 setup script

--- setup_ec2.sh
+++ setup_ec2.sh
@@ -3,6 +3,12 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
+sudo apt install -y docker.io
+sudo apt install -y docker-compose
+
+sudo systemctl enable docker
+sudo systemctl start docker
+
 sudo snap install aws-cli --classic
 
 curl -sfL https://get.k3s.io | sh -
